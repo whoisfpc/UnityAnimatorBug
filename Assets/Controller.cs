b457fd802609292f7c8a3e70d8000063e8a9e16e
whoisfpc <wo_fpc@outlook.com>
2019-07-26 20:02:26 +0800

    demo project

---
+++ Assets/Controller.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class Controller : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            GetComponent<Animator>().SetTrigger("TriggerA");
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            GetComponent<Animator>().SetTrigger("TriggerB");
+        }
+    }
+}
